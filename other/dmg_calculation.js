531a94522d838d1197ede9dbc94d78788429a9ed
bululo <ben-hur_hf@hotmail.com>
2024-11-27 03:09:35 -0300

    Oratio mudado para como deveria ser conforme a correção de seu bug nos servidores do bRO.

--- other/dmg_calculation.js
+++ other/dmg_calculation.js
@@ -92,7 +92,7 @@ function damage_calculation() {
     minMATK = Math.floor(minMATK * (multipliers.property[ALL] + multipliers.property[target.property[0]]) / 100);
     // Oratio
     if (buffs.oratio > 0) {
-        minMATK = Math.floor((minMATK * (100 + buffs.oratio))/100);
+        minMATK = Math.floor((maxMATK * (100 + (buffs.oratio*2)))/100);
     }
     minMATK = Math.floor(minMATK * (multipliers.skill_property[ALL] + multipliers.skill_property[skill.property]) / 100);
     minMATK = Math.floor(minMATK * (multipliers.race[ALL] + multipliers.race[target.race]) / 100);
@@ -104,7 +104,7 @@ function damage_calculation() {
     maxMATK = Math.floor(maxMATK * (multipliers.property[ALL] + multipliers.property[target.property[0]]) / 100);
     // Oratio
     if (buffs.oratio > 0) {
-        maxMATK = Math.floor((maxMATK * (100 + buffs.oratio))/100);
+        maxMATK = Math.floor((maxMATK * (100 + (buffs.oratio*2)))/100);
     }
     maxMATK = Math.floor(maxMATK * (multipliers.skill_property[ALL] + multipliers.skill_property[skill.property]) / 100);
     maxMATK = Math.floor(maxMATK * (multipliers.race[ALL] + multipliers.race[target.race]) / 100);
